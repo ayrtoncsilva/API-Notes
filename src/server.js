7286f1c2d4d72ff94e37714794a3c22b23b17305
Ayrton Cardoso <ayrtoncardoso80@gmail.com>
2024-01-04 11:09:40 -0300

    create table sql and migrationsRun

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require("express-async-errors");
 
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 
 const express = require("express");
 
@@ -8,13 +8,13 @@ const AppError = require("./utils/AppError")
 
 const routes = require("./routes")
 
+migrationsRun()
+
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
-
-database()
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
